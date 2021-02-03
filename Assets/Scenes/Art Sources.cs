7f0a65a788d49e0d32e62f3e88ade540c9515130
Cade <phillipscb@hendrix.edu>
2021-02-03 16:54:57 -0600

    Adding Sprites and Adding the trail to birds

---
+++ Assets/Scenes/Art Sources.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtSources : MonoBehaviour
+{
+    //https://opengameart.org/content/bearsum-pixel-art-bear
+    //https://opengameart.org/content/tree-16x16
+    //https://opengameart.org/content/red-bird
+    //https://opengameart.org/content/blended-textures-of-dirt-and-grass
+    //https://opengameart.org/content/oven
+    //https://opengameart.org/content/sky-2
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
